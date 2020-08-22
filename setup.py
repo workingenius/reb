afdbfdecefcbc647a3d75f23ba05c251cbff1ef5
ruqishang <workingenius@163.com>
2020-08-22 10:41:45 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
     name='reb',
     version='0.0.1',
-    py_modules=['reb'],
+    packages=find_packages(exclude=['tests']),
     author='ruqishang',
     author_email='workingenius@163.com',
     url='https://github.com/workingenius/reb',
