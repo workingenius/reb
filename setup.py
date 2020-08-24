17bf97311ed9f403de5df158b3af887ef1833e4d
ruqishang <ruqishang@linkdoc.com>
2020-08-24 16:40:31 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='reb',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['tests']),
     author='ruqishang',
     author_email='workingenius@163.com',
