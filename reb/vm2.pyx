b89fe4c42a764498974d3d4f35de956761a6571b
ruqishang <ruqishang@linkdoc.com>
2020-10-15 10:25:44 +0800

    Copy to vm2.pyx

--- reb/vm2.pyx
+++ reb/vm2.pyx
@@ -126,15 +126,15 @@ class InsSuccess(Instruction):
 
 
 class InsCompare(Instruction):
-    """Step to next instruction only if current character equals <ch>, otherwise current thread fails"""
+    """Step to next instruction only if current character equals <char>, otherwise current thread fails"""
     name = 'CMP'
 
-    def __init__(self, ch: str):
-        assert len(ch) <= 1
-        self.ch: str = ch
+    def __init__(self, char: str):
+        assert len(char) <= 1
+        self.char: str = char
 
     def __str__(self):
-        return '{} {}'.format(self.name, repr(self.ch))
+        return '{} {}'.format(self.name, repr(self.char))
 
 
 class InsForkHigher(InsPointer):
@@ -216,12 +216,7 @@ class Mark(object):
         )
 
 
-def _thread_id_generator():
-    i = 0
-    while True:
-        i += 1
-        yield i
-thread_id_generator = _thread_id_generator()
+_thread_id = [1]
 
 
 class Thread(object):
@@ -238,7 +233,8 @@ class Thread(object):
         self.succeed_at: int = -1
         self.moved: bool = False
 
-        self.id = next(thread_id_generator)
+        self.id = _thread_id[0]
+        _thread_id[0] += 1
 
     def to_ptnode(self, text: str) -> Optional[PTNode]:
         if not self.marks:
@@ -275,207 +271,240 @@ class Finder(BaseFinder):
         self.program: List[Instruction] = program
 
     def finditer(self, text: str) -> Iterator[PTNode]:
-        program = self.program
+        return FinderState(self.program, text)
+
+
+class FinderState(object):
+    def __init__(self, program: List[Instruction], text: str):
+        self.text: str = text
+
+        self.program: List[Instruction] = program
 
         # mapping threads pc to threads
-        thread_map: List[Optional[Thread]] = [None] * len(program)
+        self.thread_map: List[Optional[Thread]] = [None] * len(program)
 
         # current thread linked list
-        cur_hi = Thread(pc=-1, sp=-1, starter=-1)  # helper node with highest priority
-        cur_lo = Thread(pc=-1, sp=-1, starter=-1)  # helper node with lowest priority
-        cur_hi.prio_later = cur_lo
-        cur_lo.prio_former = cur_hi
+        self.cur_hi = Thread(pc=-1, sp=-1, starter=-1)  # helper node with highest priority
+        self.cur_lo = Thread(pc=-1, sp=-1, starter=-1)  # helper node with lowest priority
+        self.cur_hi.prio_later = self.cur_lo
+        self.cur_lo.prio_former = self.cur_hi
 
         # next thread linked list
-        nxt_hi = Thread(pc=-1, sp=-1, starter=-1)  # helper node with highest priority
-        nxt_lo = Thread(pc=-1, sp=-1, starter=-1)  # helper node with lowest priority
-        nxt_hi.prio_later = nxt_lo
-        nxt_lo.prio_former = nxt_hi
+        self.nxt_hi = Thread(pc=-1, sp=-1, starter=-1)  # helper node with highest priority
+        self.nxt_lo = Thread(pc=-1, sp=-1, starter=-1)  # helper node with lowest priority
+        self.nxt_hi.prio_later = self.nxt_lo
+        self.nxt_lo.prio_former = self.nxt_hi
 
-        def _print_state():
-            for i in range(len(self.program)):
-                line = '{}.\t{}'.format(i, str(self.program[i]))
-                if thread_map[i]:
-                    line += '\t<- {}'.format(str(thread_map[i]))
-                print(line)
-            
-            segs = ['cur lst: head']
-            ptr = cur_hi.prio_later
-            while ptr is not cur_lo:
-                segs.append(str(ptr))
-                ptr = ptr.prio_later
-            segs.append('tail')
-            line = ' => '.join(segs)
+        # __next__ related vars
+        self.index: int = 0
+        self.step_on: bool = True
+
+    def _print_state(self):
+        for i in range(len(self.program)):
+            line = '{}.\t{}'.format(i, str(self.program[i]))
+            if self.thread_map[i]:
+                line += '\t<- {}'.format(str(self.thread_map[i]))
             print(line)
+        
+        segs = ['cur lst: head']
+        ptr = self.cur_hi.prio_later
+        while ptr is not self.cur_lo:
+            segs.append(str(ptr))
+            ptr = ptr.prio_later
+        segs.append('tail')
+        line = ' => '.join(segs)
+        print(line)
 
-            segs = ['nxt lst: head']
-            ptr = nxt_hi.prio_later
-            while ptr is not nxt_lo:
-                segs.append(str(ptr))
-                ptr = ptr.prio_later
-            segs.append('tail')
-            line = ' => '.join(segs)
-            print(line)
+        segs = ['nxt lst: head']
+        ptr = self.nxt_hi.prio_later
+        while ptr is not self.nxt_lo:
+            segs.append(str(ptr))
+            ptr = ptr.prio_later
+        segs.append('tail')
+        line = ' => '.join(segs)
+        print(line)
 
-            print()
+        print()
 
-        def _move_thread_off(thread: Thread) -> None:
-            """Put a thread off from its linked list, nothing happens if it is not in a linked list"""
-            if thread.prio_former is not None:
-                thread.prio_former.prio_later = thread.prio_later
-            if thread.prio_later is not None:
-                thread.prio_later.prio_former = thread.prio_former
-            thread.prio_former = None
-            thread.prio_later = None
+    @staticmethod
+    def _move_thread_off(thread: Thread) -> None:
+        """Put a thread off from its linked list, nothing happens if it is not in a linked list"""
+        if thread.prio_former is not None:
+            thread.prio_former.prio_later = thread.prio_later
+        if thread.prio_later is not None:
+            thread.prio_later.prio_former = thread.prio_former
+        thread.prio_former = None
+        thread.prio_later = None
 
-        def move_thread_higher(thread: Thread, than: Thread) -> None:
-            _move_thread_off(thread)
-            assert than.prio_former is not None
-            thread.prio_later = than
-            thread.prio_former = than.prio_former
-            thread.prio_former.prio_later = thread
-            thread.prio_later.prio_former = thread
+    def move_thread_higher(self, thread: Thread, than: Thread) -> None:
+        self._move_thread_off(thread)
+        assert than.prio_former is not None
+        thread.prio_later = than
+        thread.prio_former = than.prio_former
+        thread.prio_former.prio_later = thread
+        thread.prio_later.prio_former = thread
 
-        def move_thread_lower(thread: Thread, than: Thread) -> None:
-            _move_thread_off(thread)
-            assert than.prio_later is not None
-            thread.prio_former = than
-            thread.prio_later = than.prio_later
-            thread.prio_former.prio_later = thread
-            thread.prio_later.prio_former = thread
+    def move_thread_lower(self, thread: Thread, than: Thread) -> None:
+        self._move_thread_off(thread)
+        assert than.prio_later is not None
+        thread.prio_former = than
+        thread.prio_later = than.prio_later
+        thread.prio_former.prio_later = thread
+        thread.prio_later.prio_former = thread
 
-        def del_thread(thread: Thread) -> None:
-            _move_thread_off(thread)
-            thread_map[thread.pc] = None
+    def del_thread(self, thread: Thread) -> None:
+        self._move_thread_off(thread)
+        self.thread_map[thread.pc] = None
 
-        def put_thread(thread: Thread, pc: int, expel: bool = True) -> Optional[Thread]:
-            assert thread is not None
-            if thread_map[thread.pc] is thread:
-                thread_map[thread.pc] = None
+    def put_thread(self, thread: Thread, pc: int, expel: bool = True) -> Optional[Thread]:
+        assert thread is not None
+        if self.thread_map[thread.pc] is thread:
+            self.thread_map[thread.pc] = None
 
-            to = pc
-            thread0 = thread_map[to]
-            if thread0:
-                # Should thread replace original thread0 or not
-                replace = True
-                if thread.starter > thread0.starter:
+        to = pc
+        thread0 = self.thread_map[to]
+        if thread0:
+            # Should thread replace original thread0 or not
+            replace = True
+            if thread.starter > thread0.starter:
+                replace = False
+            elif thread.starter == thread0.starter:
+                if thread0.moved:
                     replace = False
-                elif thread.starter == thread0.starter:
-                    if thread0.moved:
-                        replace = False
-                    else:
-                        replace = expel
-
-                if not replace:
-                    _move_thread_off(thread)
-                    return None
                 else:
-                    del_thread(thread0)
-            
-            thread_map[to] = thread
-            thread.pc = to
-            return thread
+                    replace = expel
 
-        def thread_for_new_char(index: int):
-            # for every new ch, create a new thread
-            # it should be the lowest priority in current linked list
-            th = Thread(pc=0, sp=index, starter=index)
-            th1 = put_thread(th, pc=th.pc)
-            if th1:
-                move_thread_higher(th1, than=cur_lo)
+            if not replace:
+                self._move_thread_off(thread)
+                return None
+            else:
+                self.del_thread(thread0)
+        
+        self.thread_map[to] = thread
+        thread.pc = to
+        return thread
 
-        def match_done(thread: Thread) -> Optional[PTNode]:
-            # found a match, arrange it to be PTNodes, and re-init threads
-            node = thread.to_ptnode(text)
-            del_thread(thread)
-            # clear all earlier started threads
-            ct = thread.succeed_at  # clear_till
-            _th1 = cur_hi.prio_later
-            assert _th1 is not None
-            _th2 = _th1.prio_later
-            while _th2 is not None:
-                if _th1.starter < ct or _th1.starter == thread.starter:
-                    del_thread(_th1)
-                _th1 = _th2
-                _th2 = _th2.prio_later
-            _th1 = nxt_hi.prio_later
-            assert _th1 is not None
-            _th2 = _th1.prio_later
-            while _th2 is not None:
-                if _th1.starter < ct or _th1.starter == thread.starter:
-                    del_thread(_th1)
-                _th1 = _th2
-                _th2 = _th2.prio_later
-            return node
+    def thread_for_new_char(self, index: int):
+        # for every new char, create a new thread
+        # it should be the lowest priority in current linked list
+        th = Thread(pc=0, sp=index, starter=index)
+        th1 = self.put_thread(th, pc=th.pc)
+        if th1:
+            self.move_thread_higher(th1, than=self.cur_lo)
 
-        def eof():
-            yield ''
+    def match_done(self, thread: Thread, text: str) -> Optional[PTNode]:
+        # found a match, arrange it to be PTNodes, and re-init threads
+        node = thread.to_ptnode(text)
+        self.del_thread(thread)
+        # clear all earlier started threads
+        ct = thread.succeed_at  # clear_till
+        _th1 = self.cur_hi.prio_later
+        assert _th1 is not None
+        _th2 = _th1.prio_later
+        while _th2 is not None:
+            if _th1.starter < ct or _th1.starter == thread.starter:
+                self.del_thread(_th1)
+            _th1 = _th2
+            _th2 = _th2.prio_later
+        _th1 = self.nxt_hi.prio_later
+        assert _th1 is not None
+        _th2 = _th1.prio_later
+        while _th2 is not None:
+            if _th1.starter < ct or _th1.starter == thread.starter:
+                self.del_thread(_th1)
+            _th1 = _th2
+            _th2 = _th2.prio_later
+        return node
+
+    def __iter__(self) -> Iterator[PTNode]:
+        return self
+
+    def __next__(self) -> PTNode:
+        node: Optional[PTNode] = None
+
+        text = self.text
+        program = self.program
+        thread_map = self.thread_map
 
         th: Optional[Thread]
         ins: Instruction
 
-        for index, ch in enumerate(chain(iter(text), eof())):
-            thread_for_new_char(index)
+        while node is None:
+            index = self.index
+            if index == len(text):
+                char = ''
+            elif index > len(text):
+                raise StopIteration
+            else:
+                char = text[index]
+
+            if self.step_on:
+                self.thread_for_new_char(index)
+            self.step_on = True
 
             # as long as the ready ll is not empty
-            while cur_hi.prio_later is not cur_lo:
+            while self.cur_hi.prio_later is not self.cur_lo:
                 if DEBUG:
-                    _print_state()
+                    self._print_state()
 
                 # pick the ready thread with highest prioity and run it
-                th = cur_hi.prio_later
+                th = self.cur_hi.prio_later
                 assert th is not None
                 ins = program[th.pc]
 
                 if isinstance(ins, InsStart):
-                    put_thread(th, pc=th.pc + 1)
+                    self.put_thread(th, pc=th.pc + 1)
                 elif isinstance(ins, InsSuccess):
                     if th.succeed_at < 0:
                         th.succeed_at = index
-                    if nxt_hi.prio_later is nxt_lo:
-                        node = match_done(th)
+                    if self.nxt_hi.prio_later is self.nxt_lo:
+                        assert node is None
+                        node = self.match_done(th, text)
                         assert node is not None
-                        yield node
                     else:
-                        move_thread_higher(th, than=nxt_lo)
+                        self.move_thread_higher(th, than=self.nxt_lo)
                 elif isinstance(ins, InsCompare):
-                    move_thread_higher(th, than=nxt_lo)
+                    self.move_thread_higher(th, than=self.nxt_lo)
                     th.moved = True
                 elif isinstance(ins, InsForkHigher):
                     th1 = Thread(pc=ins.to, sp=index, starter=th.starter, marks=th.marks)
-                    if put_thread(th1, pc=th1.pc):
-                        move_thread_higher(th1, than=th)
-                    put_thread(th, pc=th.pc + 1)
+                    if self.put_thread(th1, pc=th1.pc):
+                        self.move_thread_higher(th1, than=th)
+                    self.put_thread(th, pc=th.pc + 1)
                 elif isinstance(ins, InsForkLower):
                     th1 = Thread(pc=ins.to, sp=index, starter=th.starter, marks=th.marks)
-                    if put_thread(th1, pc=th1.pc):
-                        move_thread_lower(th1, than=th)
-                    put_thread(th, pc=th.pc + 1)
+                    if self.put_thread(th1, pc=th1.pc):
+                        self.move_thread_lower(th1, than=th)
+                    self.put_thread(th, pc=th.pc + 1)
                 elif isinstance(ins, InsJump):
-                    put_thread(th, pc=ins.to)
+                    self.put_thread(th, pc=ins.to)
                 elif isinstance(ins, InsGroupStart):
                     th.marks.append(Mark(index=index, name=ins.group_id, is_open=True, depth=0))  # TODO depth
-                    put_thread(th, pc=th.pc + 1)
+                    self.put_thread(th, pc=th.pc + 1)
                 elif isinstance(ins, InsGroupEnd):
                     th.marks.append(Mark(index=index, name=ins.group_id, is_open=False, depth=0))  # TODO depth
-                    put_thread(th, pc=th.pc + 1)
+                    self.put_thread(th, pc=th.pc + 1)
                 elif isinstance(ins, InsPredicate):
-                    move_thread_higher(th, than=nxt_lo)
+                    self.move_thread_higher(th, than=self.nxt_lo)
                     th.moved = True
                 elif isinstance(ins, InsAny):
-                    move_thread_higher(th, than=nxt_lo)
+                    self.move_thread_higher(th, than=self.nxt_lo)
                     th.moved = True
                 elif isinstance(ins, InsAssert):
-                    if ins.pred(ch, index, text):
-                        if put_thread(th, pc=th.pc + 1):
-                            move_thread_higher(th, than=cur_lo)
+                    if ins.pred(char, index, text):
+                        if self.put_thread(th, pc=th.pc + 1):
+                            self.move_thread_higher(th, than=self.cur_lo)
                     else:
-                        del_thread(th)
+                        self.del_thread(th)
                 else:
                     raise TypeError('Invalid Instruction Type')
 
+                if node is not None:
+                    self.step_on = False
+                    return node
+
             if DEBUG:
-                _print_state()
+                self._print_state()
 
             # for each alive thread, step on
             # and reset moved flag to "not moved"
@@ -489,33 +518,42 @@ class Finder(BaseFinder):
                 if isinstance(ins, InsSuccess):
                     pass
                 elif isinstance(ins, InsCompare):
-                    if ins.ch == ch:
-                        put_thread(th, pc=th.pc + 1, expel=False)
+                    if ins.char == char:
+                        self.put_thread(th, pc=th.pc + 1, expel=False)
                     else:
-                        del_thread(th)
+                        self.del_thread(th)
                 elif isinstance(ins, InsPredicate):
-                    if ins.pred(ch, index, text):
-                        put_thread(th, pc=th.pc + 1, expel=False)
+                    if ins.pred(char, index, text):
+                        self.put_thread(th, pc=th.pc + 1, expel=False)
                     else:
-                        del_thread(th)
+                        self.del_thread(th)
                 elif isinstance(ins, InsAny):
-                    put_thread(th, pc=th.pc + 1, expel=False)
+                    self.put_thread(th, pc=th.pc + 1, expel=False)
                 else:
                     raise TypeError
 
             if DEBUG:
-                _print_state()
+                self._print_state()
 
             # swap cur list and next list
-            cur_hi, cur_lo, nxt_hi, nxt_lo = nxt_hi, nxt_lo, cur_hi, cur_lo
+            self.cur_hi, self.cur_lo, self.nxt_hi, self.nxt_lo = self.nxt_hi, self.nxt_lo, self.cur_hi, self.cur_lo
 
-        th = thread_map[-1]
-        if th is not None:
-            ins = program[th.pc]
-            assert isinstance(ins, InsSuccess)
-            node = match_done(th)
-            assert node is not None
-            yield node
+            # ending
+            if index == len(text):
+                th = thread_map[-1]
+                if th is not None:
+                    ins = program[th.pc]
+                    assert isinstance(ins, InsSuccess)
+                    assert node is None
+                    node = self.match_done(th, text)
+                    assert node is not None
+                
+                if node is None:
+                    raise StopIteration
+        
+            self.index += 1
+
+        return node
 
 
 def compile_pattern(pattern: Pattern) -> Finder:
