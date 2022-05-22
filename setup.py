d7543b99709f15403304cb415c34e36bdc0df327
ruqishang <workingenius@163.com>
2022-05-22 14:09:48 +0800

    Try publish with github action

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from setuptools import setup, find_packages, Extension
+# from Cython.Build import cythonize
 
 projdir = Path(__file__).parent
 readme = (projdir / 'README.md').read_text()
 
 setup(
     name='reb',
-    version='0.1.1',
+    version='0.1.1.0',
     packages=find_packages(exclude=['tests']),
     author='ruqishang',
     author_email='workingenius@163.com',
@@ -21,5 +22,6 @@ setup(
     },
     long_description=readme,
     long_description_content_type='text/markdown',
-    ext_modules=[Extension('reb.vm.vm2', ['reb/vm/vm2.c'])]
+    ext_modules=[Extension('reb.vm.vm2', ['reb/vm/vm2.c'])],
+    # ext_modules=cythonize(['reb/vm/vm2.pyx', ])
 )
